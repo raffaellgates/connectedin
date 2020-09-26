4b91d5f85fe12b256241bc6b83e012d0eb01f8ae
rafaGates <rafael12silva73@gmail.com>
2020-09-26 00:39:17 -0300

    corrigindo erros

--- perfis/models.py
+++ perfis/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
-
 class Perfil(models.Model):
-    nome = models.CharField(max_length=255, null=False)
-    email = models.CharField(max_length=255, null=False)
-    telefone = models.CharField(max_length=15, null=False)
-    nome_empresa = models.CharField(max_length=255, null=False)
+	nome = models.CharField(max_length=255, null=False)
+	email = models.EmailField(max_length=100, null=False)
+	telefone = models.CharField(max_length=15, null=False)
+	nome_empresa = models.CharField(max_length=80, null=False)
+
+	def convidar(self, perfil_convidado):
+		convite = Convite(solicitados=self,
+							recebidos=perfil_convidado)
+		convite.save()
+
+class Convite(models.Model):
+	solicitados = models.ForeignKey(Perfil,
+		related_name = 'solicitados',
+		on_delete = models.CASCADE)
+	recebidos = models.ForeignKey(Perfil, 
+		related_name = 'recebidos',
+		on_delete = models.CASCADE)
