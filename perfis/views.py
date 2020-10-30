599dc22c3474d6f2a99fc5689343ea57bf1c686a
rafaGates <rafael12silva73@gmail.com>
2020-10-30 18:00:17 -0300

    corrigindo erros

--- perfis/views.py
+++ perfis/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
-from perfis import models
+from perfis.models import Perfil
 from django.shortcuts import redirect
+# Create your views here.
 
 def index(request):
-	perfil = models.Perfil.objects.all()
-	return render(request, 'index.html',{'perfil' : perfil})
+	return render(request, 'index.html',{'perfis' :Perfil.objects.all()})
 
-def exibir(request, perfil_id):
-	perfil = models.Perfil.objects.get(id=perfil_id)
-	return render(request, 'perfil.html',{'perfil' : perfil})
+def exibir_perfil(request, perfil_id):
+	perfil = Perfil.objects.get(id=perfil_id)
 
-def convidar(request, perfil_id):
-	perfil_convidado = models.Perfil.objects.get(id=perfil_id)
+	return render(request, 'perfil.html',
+		          {'perfil' : perfil})
+
+def  convidar(request,perfil_id):
+
+	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
 	perfil_logado = get_perfil_logado(request)
-	perfil_logado.convidar(perfil_convidado)
-	return redirect('index')
+	perfil_logado.convidar(perfil_a_convidar)
+
+	return  redirect('index')
 
 def get_perfil_logado(request):
-	return models.Perfil.objects.get(id=1)
+	return Perfil.objects.get(id=1)
