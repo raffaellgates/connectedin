cb706ff73d70dbb3069a6f7cd220f46066e9203b
rafaGates <rafael12silva73@gmail.com>
2020-10-31 00:54:43 -0300

    corrigindo

--- perfis/models.py
+++ perfis/models.py
@@ -11,10 +11,17 @@ class Perfil(models.Model):
 
     nome_empresa = models.CharField(max_length=255, null=False)
 
+    contatos = models.ManyToManyField('self')
+    
     def convidar(self, perfil_convidado):
-        convite = Convite(solicitante=self,convidado = perfil_convidado)
-        convite.save()
+        Convite(solicitante=self, convidado=perfil_convidado).save()
+
 
 class Convite(models.Model):
     solicitante = models.ForeignKey(Perfil,on_delete=models.CASCADE,related_name='convites_feitos' )
     convidado = models.ForeignKey(Perfil, on_delete= models.CASCADE, related_name='convites_recebidos')
+    
+    def aceitar(self):
+        self.convidado.contatos.add(self.solicitante)
+        self.solicitante.contatos.add(self.convidado)
+        self.delete()
--- perfis/views.py
+++ perfis/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
-from perfis.models import Perfil
+from perfis.models import Perfil, Convite
 from django.shortcuts import redirect
 # Create your views here.
 
 def index(request):
-	return render(request, 'index.html',{'perfis' :Perfil.objects.all()})
+	return render(request, 'index.html',
+					{'perfis' : Perfil.objects.all(),
+					'perfil_logado' : get_perfil_logado(request)})
 
 def exibir_perfil(request, perfil_id):
 	perfil = Perfil.objects.get(id=perfil_id)
+	perfil_logado = get_perfil_logado(request)
+	ja_eh_contato = perfil in perfil_logado.contatos.all()
 
 	return render(request, 'perfil.html',
-		          {'perfil' : perfil})
+						{'perfil' : perfil,
+						'perfil_logado' : get_perfil_logado(request),
+						'ja_eh_contato' : ja_eh_contato})
+	
 
 def  convidar(request,perfil_id):
 
@@ -22,3 +29,8 @@ def  convidar(request,perfil_id):
 
 def get_perfil_logado(request):
 	return Perfil.objects.get(id=1)
+
+def aceitar(request, convite_id):
+	convite = Convite.objects.get(id=convite_id)
+	convite.aceitar()
+	return redirect('index')
